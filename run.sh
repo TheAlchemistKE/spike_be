0f692dbd18d791484a48f1b6499786dd8f1e3f1c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-07-19 17:26:09 +0300

    add project entrypoint

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+
+cd ./analytics_service && npm run dev
+cd ./auth_service && npm run dev
+cd ./business_service && npm run dev
+cd ./delivery_service && npm run dev
+cd ./payment_service && npm run dev
+cd ./user_service && npm run dev
