410ececa255b0f8e16e61ed201d087d895ab59ad
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-07-21 18:41:43 +0300

    refactor auth routes

--- auth_service/src/routes/auth.ts
+++ auth_service/src/routes/auth.ts
@@ -1,5 +1,13 @@
-import {Router} from "express";
+import express from 'express';
+import { authenticate } from '../middleware/auth';
+import {AuthController} from "../controllers/auth";
 
-const router = Router;
+const router = express.Router();
+const authController = new AuthController();
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/refresh-token', authController.refreshToken);
+router.post('/logout', authenticate, authController.logout);
 
 export default router;
