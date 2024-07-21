b4c892bf064f880f76ad57ca283a3059a88c296c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-07-21 15:07:02 +0300

    add profile entity

--- auth_service/src/database/index.ts
+++ auth_service/src/database/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import {DataSource} from "typeorm";
 import {User} from "./models/user";
 import {BlacklistedTokens} from "./models/blacklisted_tokens";
+import {Profile} from "./models/profile";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -10,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: "postgres",
     password: "",
     database: "spike_userbase",
-    entities: ["./models/*.ts"],
+    entities: [User, Profile, BlacklistedTokens],
     synchronize: true,
     logging: false,
     poolSize: 10,
