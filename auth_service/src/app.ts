5274751e9bc2bb85593a358be38c5b429d21756c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-07-21 14:27:42 +0300

    add express configs

---
+++ auth_service/src/app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import { Server} from 'socket.io'
+import dotenv from 'dotenv';
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser"
+import { io as auth_emitter } from "socket.io-client";
+import "reflect-metadata"
+import http from "http";
+import auth from "./routes/auth";
+import {AppDataSource} from "./database";
+dotenv.config();
+
+
+
+export default async (port: number) => {
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+            allowedHeaders: ["Authorization", "Content-Type", "App", "Accept"],
+            // credentials: true
+        },
+    });
+
+    const auth_socket = auth_emitter('', {
+        transports: ["websocket"],
+        //@ts-ignore
+        pingInterval: 60000,
+        pingTimeout: 60000,
+        upgradeTimeout: 30000,
+    })
+
+
+    // Apply Middlewares
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser())
+    app.use(helmet());
+
+
+
+
+    return app.listen(port, async () => {
+        await AppDataSource.initialize()
+        console.log(`Server running on port ${port}`);
+    })
+
+}
